df4c2aba77c1812d9356f5ad146cc638c33dc36f
RozeQz <liza.roz@yandex.ru>
2022-05-31 01:21:18 +0300

    Finish dz2

--- dz2/main.cs
+++ dz2/main.cs
@@ -4,19 +4,17 @@
 
 using namespace std;
 
-const set<char> sl_symbol{',', '*', ';', '(', ')', '!', '/', '\\', '.', '^', '#', '@', '$', '&', ' '};
-const set<char> digit{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-const set<string> type{"char", "int", "float", "double", "bool", "short", "long"};
-const set<string> sl_words{"void", "char", "int", "float", "double", "bool", "short", "long", "switch",
-                           "true", "false", "return", "for", "while", "if", "else", "auto", "break",
-                           "case", "catch", "class", "const", "continue", "default", "delete", "do",
-                           "enum", "extern", "friend", "goto", "inline", "new", "operator", "private",
-                           "protected", "public", "register", "signed", "sizeof", "static", "struct",
-                           "template", "this", "throw", "try", "typedef", "union", "unsigned", "virtual", "volatile"};
-set<char> alphabet;
+const set<char> sl_symbol{ ',', '*', ';', '(', ')', '!', '/', '\\', '.', '^', '#', '@', '$', '&', ' '};
+const set<char> digit{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+const set<string> type{ "char", "int", "float", "double", "bool", "short", "long", "void" };
+const set<string> sl_words{ "void", "char", "int", "float", "double", "bool", "short", "long", "switch",
+                            "true", "false", "return", "for", "while", "if", "else", "auto", "break",
+                            "case", "catch", "class", "const", "continue", "default", "delete", "do",
+                            "enum", "extern", "friend", "goto", "inline", "new", "operator", "private",
+                            "protected", "public", "register", "signed", "sizeof", "static", "struct",
+                            "template", "this", "throw", "try", "typedef", "union", "unsigned", "virtual", "volatile" };
 
-void delSpace(string &str)
-{ // Удаление лишних пробелов
+void delSpace(string& str) { // РЈРґР°Р»РµРЅРёРµ Р»РёС€РЅРёС… РїСЂРѕР±РµР»РѕРІ
     int d = str.length();
     for (int i = 0; i < d; i++)
     {
@@ -31,221 +29,151 @@ void delSpace(string &str)
             str.erase(i, 1);
         }
     }
-    if (str.length())
-    {
-        while (str[str.length() - 1] == ' ')
-        {
-            str.erase(str.length() - 1, 1);
-        }
+    if (str.length()) {
+        while (str[str.length() - 1] == ' ') { str.erase(str.length() - 1, 1); }
     }
 }
 
 inline bool isSymbol(char x) { return (('a' <= x && x <= 'z') || ('A' <= x) && (x <= 'Z') || (x == '_')); }
 
-bool isSlSymbol(char x)
-{
-    if (count(sl_symbol.begin(), sl_symbol.end(), x))
-        return true;
+bool isSlSymbol(char x) {
+    if (count(sl_symbol.begin(), sl_symbol.end(), x)) return true;
     return false;
 }
 
-bool isType(string str)
-{
-    if (count(type.begin(), type.end(), str))
-    {
-        cout << "Тип: " << str << endl;
-        return true;
-    }
-    cout << "Неизвестный тип: " << str << endl;
+bool isDigit(char x) {
+    if (count(digit.begin(), digit.end(), x)) return true;
     return false;
 }
 
-bool isDigit(char x)
-{
-    if (count(digit.begin(), digit.end(), x))
-        return true;
-    return false;
+void typeWord(string str) {
+    if (count(type.begin(), type.end(), str)) cout << "РўРёРї: \t\t\t\"" << str << '\"' << endl;
+    else throw string("РќРµРёР·РІРµСЃС‚РЅС‹Р№ С‚РёРї: \t\"").append(str + '"');
 }
 
-bool isIdentificator(string str)
-{
-    if (str.empty())
-    {
-        throw string("Ожидался идентификатор");
-        return false;
+void voidWord(string str) {
+    if (str != "void") throw string("РћР¶РёРґР°Р»РѕСЃСЊ РІСЃС‚СЂРµС‚РёС‚СЊ: \"void\", Р° РІСЃС‚СЂРµС‡РµРЅРѕ: \"").append(str + '"');
+    else cout << "РЎР»СѓР¶РµР±РЅРѕРµ СЃР»РѕРІРѕ: \t\"" << str << '\"' << endl;
+}
+
+void parenOpen(string str) {
+    if (str != "(") throw string("РћР¶РёРґР°Р»РѕСЃСЊ РІСЃС‚СЂРµС‚РёС‚СЊ: \t\'(\'");
+    else cout << "РЎР»СѓР¶РµР±РЅС‹Р№ СЃРёРјРІРѕР»: \t\"" << str << '\"' << endl;
+}
+
+void parenClose(string str) {
+    if (str != ")") throw string("РћР¶РёРґР°Р»РѕСЃСЊ РІСЃС‚СЂРµС‚РёС‚СЊ: \t\')\'");
+    else cout << "РЎР»СѓР¶РµР±РЅС‹Р№ СЃРёРјРІРѕР»: \t\"" << str << '\"' << endl;
+}
+
+void semicolon(string &str) {
+    if (str != ";") throw string("РћР¶РёРґР°Р»РѕСЃСЊ РІСЃС‚СЂРµС‚РёС‚СЊ: \t\';\'");
+    else cout << "РЎР»СѓР¶РµР±РЅС‹Р№ СЃРёРјРІРѕР»: \t\"" << str << '\"' << endl;
+}
+
+void star(string str) {
+    if (str != "*") throw string("РћР¶РёРґР°Р»РѕСЃСЊ РІСЃС‚СЂРµС‚РёС‚СЊ: \t\'*\'");
+    else cout << "РЎР»СѓР¶РµР±РЅС‹Р№ СЃРёРјРІРѕР»: \t\"" << str << '\"' << endl;
+}
+
+void comma(string str) {
+    if (str != ",") throw string("РћР¶РёРґР°Р»РѕСЃСЊ РІСЃС‚СЂРµС‚РёС‚СЊ: \t\',\'");
+    else cout << "РЎР»СѓР¶РµР±РЅС‹Р№ СЃРёРјРІРѕР»: \t\"" << str << '\"' << endl;
+}
+
+void identificator(string str) {
+    if (str.empty()) throw string("РћР¶РёРґР°Р»СЃСЏ РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂ");
+    if (count(sl_words.begin(), sl_words.end(), str)) throw string("РќРµРґРѕРїСѓСЃС‚РёРјРѕРµ РёРјСЏ РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂР°: \"").append(str + '"');
+    if (!isSymbol(str[0])) throw string("РќРµРґРѕРїСѓСЃС‚РёРјРѕРµ РёРјСЏ РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂР°: \"").append(str + '"');
+    for (int i = 1; i < str.length(); i++) {
+        if (isSlSymbol(str[i])) throw string("РќРµРґРѕРїСѓСЃС‚РёРјРѕРµ РёРјСЏ РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂР°: \"").append(str + '"');
     }
-    if (count(sl_words.begin(), sl_words.end(), str))
-    {
-        throw string("Недопустимое имя идентификатора: ").append(str);
-        return false;
-    }
-    if (!isSymbol(str[0]))
-    {
-        throw string("Недопустимое имя идентификатора: ").append(str);
-        ;
-        return false;
-    }
-    for (int i = 1; i < str.length(); i++)
-    {
-        if (isSlSymbol(str[i]))
-        {
-            throw string("Недопустимое имя идентификатора: ").append(str);
-            ;
-            return false;
+    cout << "РРґРµРЅС‚РёС„РёРєР°С‚РѕСЂ: \t\t\"" << str << '\"' << endl;
+}
+
+void paramFunc(string str) {
+    delSpace(str);
+    if (!str.empty()) {
+        int n = count(str.begin(), str.end(), ',');
+        typeWord(str.substr(0, str.find(' ')));
+        str.erase(0, str.find(' ') + 1);
+        if (n == 0) {
+            identificator(str.substr(0, str.find('\0')));
+            str.erase(0, str.find('\0'));
+        }
+        else {
+            identificator(str.substr(0, str.find_first_of(',')));
+            str.erase(0, str.find_first_of(','));
+            for (int i = 0; i < n; i++) {
+                comma(str.substr(0, 1));
+                str.erase(0, 1);
+                paramFunc(str.substr(0, str.find('\n')));
+            }
         }
     }
-    cout << "Идентификатор: " << str << endl;
-    return true;
 }
 
-bool isVoid(string str)
-{
-    if (str != "void")
-    {
-        throw string("Ожидалось встретить: void, а встречено: ").append(str);
-        return false;
-    }
-    cout << "Служебное слово: " << str << endl;
-    return true;
-}
-
-bool isParenOpen(string str)
-{
-    if (str != "(")
-    {
-        throw string("Ожидалось встретить: \'(\'");
-        return false;
-    }
-    cout << "Служебный символ: " << str << endl;
-    return true;
-}
-
-bool isParenClose(string str)
-{
-    if (str != ")")
-    {
-        throw string("Ожидалось встретить: \')\'");
-        return false;
-    }
-    cout << "Служебный символ: " << str << endl;
-    return true;
-}
-
-bool isSemicolon(string str)
-{
-    if (str != ";")
-    {
-        throw("Ожидалось встретить: \';\'");
-        return false;
-    }
-    cout << "Служебный символ: " << str << endl;
-    return true;
-}
-
-bool isStar(string str)
-{
-    if (str != "*")
-    {
-        throw("Ожидалось встретить: \'*\'");
-        return false;
-    }
-    cout << "Служебный символ: " << str << endl;
-    return true;
-}
-
-bool isParamFunc(string str)
-{
+void paramProc(string str) {
     delSpace(str);
-    if (str.empty())
-        return true;
-
-    if (!isType(str.substr(0, str.find(' '))))
-        return false;
-    str.erase(0, str.find(' ') + 1);
-    if (!isIdentificator(str.substr(0, str.find_first_of(',', '\0'))))
-        return false;
-    str.erase(0, str.find_first_of(',', '\n'));
-    return true;
+    if (!str.empty()) {
+        typeWord(str.substr(0, str.find(' ')));
+        str.erase(0, str.find(' ') + 1);
+        parenOpen(str.substr(0, 1));
+        str.erase(0, 1);
+        star(str.substr(0, 1));
+        str.erase(0, 1);
+        identificator(str.substr(0, str.find(')')));
+        str.erase(0, str.find(')'));
+        parenClose(str.substr(0, 1));
+        str.erase(0, 1);
+        parenOpen(str.substr(0, 1));
+        str.erase(0, 1);
+        paramFunc(str.substr(0, str.find_first_of(')')));
+        str.erase(0, str.find_first_of(')'));
+        parenClose(str.substr(0, 1));
+        str.erase(0, 1);
+        if (!str.empty()) {
+            comma(str.substr(0, 1));
+            str.erase(0, 1);
+            paramProc(str.substr(0, str.find('\n')));
+        }
+    }
 }
 
-bool isParamProc(string str)
-{
-    delSpace(str);
-    if (str.empty())
-        return true;
-
-    if (!isType(str.substr(0, str.find(' '))))
-        return false;
-    str.erase(0, str.find(' ') + 1);
-    if (!isParenOpen(str.substr(0, 1)))
-        return false;
-    str.erase(0, 1);
-    if (!isStar(str.substr(0, 1)))
-        return false;
-    str.erase(0, 1);
-    if (!isIdentificator(str.substr(0, str.find(')'))))
-        return false;
-    str.erase(0, str.find(')'));
-    if (!isParenClose(str.substr(0, 1)))
-        return false;
-    str.erase(0, 1);
-    if (!isParenOpen(str.substr(0, 1)))
-        return false;
-    str.erase(0, 1);
-    if (!isParamFunc(str.substr(0, str.find_last_of(')'))))
-        return false;
-    str.erase(0, str.find_last_of(')'));
-    if (!isParenClose(str.substr(0, 1)))
-        return false;
+void procedure(string str) {
+    voidWord(str.substr(0, str.find(' ')));
+    str.erase(0, str.find(' '));
     str.erase(0, 1);
 
-    return true;
-}
-
-bool isProcedure(string str)
-{
-    if (!isVoid(str.substr(0, str.find(' '))))
-        return false;
-    str.erase(0, str.find(' ') + 1);
-    if (!isIdentificator(str.substr(0, str.find('('))))
-        return false;
+    identificator(str.substr(0, str.find('(')));
     str.erase(0, str.find('('));
-    if (!isParenOpen(str.substr(0, 1)))
-        return false;
-    str.erase(0, 1);
-    if (!isParamProc(str.substr(0, str.find_last_of(')'))))
-        return false;
-    str.erase(0, str.find_last_of(')'));
-    if (!isParenClose(str.substr(0, 1)))
-        return false;
-    str.erase(0, 1);
-    if (!isSemicolon(str.substr(0, str.find('\0'))))
-        return false;
-    return true;
-}
 
-void analyze(string str)
-{
-    cout << isProcedure(str) << endl;
+    parenOpen(str.substr(0, 1));
+    str.erase(0, 1);
+
+    paramProc(str.substr(0, str.find_last_of(')')));
+    str.erase(0, str.find_last_of(')'));
+
+    parenClose(str.substr(0, 1));
+    str.erase(0, 1);
+
+    semicolon(str);
+    str.erase(0, 1);
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     string str;
-    while (cout << "Enter a string:\n", getline(cin, str), str != "end")
-    {
+    while (cout << "Р’РІРµРґРёС‚Рµ СЃС‚СЂРѕРєСѓ:\n", getline(cin, str), str != "end") {
         delSpace(str);
-        try
-        {
-            analyze(str);
-            cout << "\n\nCORRECT!\n"
-                 << endl;
+        try {
+            procedure(str);
+            cout << "\nРџСЂР°РІРёР»СЊРЅРѕ!\n" << endl;
         }
-        catch (const string str)
-        {
-            cerr << str << "\n\nINCORRECT\n"
-                 << endl;
+        catch (const string str) {
+            cerr << str << "\nРќРµРІРµСЂРЅРѕ!\n" << endl;
         }
+        cout << "Р•СЃР»Рё Р’С‹ С…РѕС‚РёС‚Рµ Р·Р°РєРѕРЅС‡РёС‚СЊ, РІРІРµРґРёС‚Рµ \"end\".\n" << endl;
     }
 }
